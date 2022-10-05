b229347b2321117faef30bf3f69d8404ec6d5e53
Sampath Kumar Bidre <56542248+bidresam@users.noreply.github.com>
2022-10-05 18:07:32 +0530

    Update forms.py

--- django-elk-main/django_elk/base_app/forms.py
+++ django-elk-main/django_elk/base_app/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CityAddForm(forms.Form):
     city_name = forms.CharField(label='City')
-    city_population = forms.IntegerField(label='Polulation', min_value=1)
+    city_population = forms.IntegerField(label='Population', min_value=1)
